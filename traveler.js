b611ff4d90d36427407f7f3a5b69010f560cc227
Vladislav <vlad1010@inbox.ru>
2013-10-06 01:07:06 +0600

    Оптимизация алгоритма сортировки

--- traveler.js
+++ traveler.js
@@ -5,56 +5,133 @@ function Traveler(options) {
 
 Traveler.prototype = {
 	defaultOptions: {
-		cards: Array(),
+		cards: new Array(),
+		transports: new Array(),
 	},
-	options: {
-		
-	},
-
+	options: {},
+	
+	transports: new Array('airplane', 'trane', 'bus', 'taxi'),
+	cards: new Array(),
+	
 	init: function(options) {
+		this.options = (options) ? options : {};
+
 		for(var option in this.defaultOptions) {
-			this.options[option] = options && (options[option] !== undefined) ? options[option] : this.defaultOptions[option];
+			this.options[option] = (options && typeof(options[option]) !== undefined) ? options[option] : this.defaultOptions[option];
 		};
+		
+		//TODO: можно добавить валидацию транспорта
+		this.transports = this.transports.concat(this.options['transports']);
+		
+		//TODO: можно добавить валидацию карточек
+		this.cards = this.options['cards'];
 	},
 	
-	/* Получить список карточек по условия или без 
-	 * options: {'start': undefined | 0, 'end': undefined | 4, 'limit':1, 'to':undefined | 'Madrid', 'from':undefined | 'Barcelona', 'sent': undefined | '45C'}
-	 * 
-	 * 
+	/* Получить список карточек
+	 * Поиск по одному свойству
+	 * options: {
+	 * 	'start': undefined | 0,
+	 * 	'end': undefined | 4,
+	 * 	'limit': undefined | 1, 
+	 * 	'to':undefined | 'Madrid',
+	 * 	'from':undefined | 'Barcelona',
+	 * 	'sent': undefined | '45C',
+	 * 	'transport': undefined | 'airplane',
+	 * 	'options': undefined | {'key':'price', 'display':true|false и т.д.},
+	 * }
+	 * @param object
+	 * @return array
 	 * */
-	getCards: function(options) {
+	getCards: function(search_options) {
 		var search_cards = new Array();
+		var search_options = (typeof(search_options) === 'object') ? search_options : {};
+		var push_limit = 0;
 		
-		var start = (typeof(options['start']) == 'int') ? options['start'] : 0;
-		var end = (typeof(options['end']) == 'int') ? options['end'] : this.options['cards'];
-		var to = (typeof(options['to']) == 'string') ? options['to'] : undefined;
-		var from = (typeof(options['from']) == 'string') ? options['from'] : undefined;
-		var sent = (typeof(options['sent']) == 'string') ? options['sent'] : undefined;
-		
+		var start = (typeof(search_options['start']) === 'int') ? search_options['start'] : 0;
+		var end = (typeof(search_options['end']) === 'int') ? search_options['end'] : this.getCountCards();
+		var limit = (typeof(search_options['limit']) === 'int' && search_options['limit'] > 0) ? search_options['limit'] : 0;
+		var to = (typeof(search_options['to']) === 'string') ? search_options['to'] : undefined;
+		if (to) {
+			push_limit++;
+		}
+		var from = (typeof(search_options['from']) === 'string') ? search_options['from'] : undefined;
+		if (from) {
+			push_limit++;
+		}
+		var id = (typeof(search_options['id']) === 'string') ? search_options['id'] : undefined;
+		if (id) {
+			push_limit++;
+		}
+		var sent = (typeof(search_options['sent']) === 'string') ? search_options['sent'] : undefined;
+		if (sent) {
+			push_limit++;
+		}
+		var transport = (typeof(search_options['transport']) === 'string') ? search_options['transport'] : undefined;
+		if (transport) {
+			push_limit++;
+		}
+		var options = (typeof(search_options['options']) === 'object') ? search_options['options'] : undefined;
+		if (options) {
+			push_limit++;
+		}
+
 		for(var i=start; i<end; i++) {
-			if (typeof(options['limit']) == 'int' && cards.length === options['limit']) {
+			if (limit !== 0 && search_cards.length === limit) {
 				break;
 			}
 			
-			if (to && this.options['cards'][i]['to'] === to) {
-				search_cards.push(this.options['cards'][i]);
-				continue;
+			var push = 0;
+			
+			if (to && this.cards[i]['to'] === to) {
+				push++;
 			}
-			if (from && this.options['cards'][i]['from'] === from) {
-				search_cards.push(this.options['cards'][i]);
-				continue;
+			if (from && this.cards[i]['from'] === from) {
+				push++;
 			}
-			if (sent && this.options['cards'][i]['sent'] === sent) {
-				search_cards.push(this.options['cards'][i]);
-				continue;
+			if (id && this.cards[i]['id'] === id) {
+				push++;
+			}
+			if (sent && this.cards[i]['sent'] === sent) {
+				push++;
+			}
+			if (transport && this.cards[i]['transport'] === transport) {
+				push++;
+			}
+			if (options && typeof(this.cards[i]['options']) === 'object') {
+				if (options['key'] && this.cards[i]['options'][key] === options['key']) {
+					push++;
+				}
+				
+				for(var key_option in options) {
+					if (key_option === 'key') {
+						continue;
+					}
+					for(var key in this.cards[i]['options']) {
+						if (this.cards[i]['options'][key][key_option] && this.cards[i]['options'][key][key_option] === options[key_option]) {
+							push++;
+						}
+					}
+				}
+			}
+			
+			if (push >= push_limit) {
+				search_cards.push(this.cards[i]);
 			}
 		}
 		
 		return search_cards;
 	},
-
 	
-	/* Валидация карточки */
+	/* Число карточек 
+	 * @return int
+	 * */
+	getCountCards: function() {
+		return this.cards.length;
+	},
+	
+	/* Валидация карточки 
+	 * @return bool
+	 * */
 	validate: function(card) {
 		if (!card || typeof(card) != 'object') {
 			throw {name: 'Type', message: 'Не верный тип данных'};
@@ -71,11 +148,14 @@ Traveler.prototype = {
 		//TODO: добавить проверку на отсутствие дырки в карточках при добавлении новой
 	},
 	
-	/* Добавление карточки */
+	/* Добавление карточки 
+	 * @param object
+	 * @return bool
+	 * */
 	addCard: function(card) {
 		try {
 			this.validate(card);
-			this.options['cards'].push(card);
+			this.cards.push(card);
 		} catch(error) {
 			throw error;
 			return false;
@@ -84,17 +164,22 @@ Traveler.prototype = {
 		return true;
 	},
 	
-	/* Удаление карточки */
+	/* Удаление карточки 
+	 * @param object
+	 * @return bool
+	 * */
 	removeCard: function(card) {
-		//TODO: По каким условиям удалять? 
+		//Удаляем по условиям аналогичным getCards 
 	},
 	
-	/* Сортировать карточки в порядке путешествия */
+	/* Сортировка
+	 * @return array
+	 * */
 	sort: function() {
 		//Сортированный список карточек
 		var sort_cards = new Array();
-		
-		var count_cards = this.options['cards'].length;
+
+		var count_cards = this.getCountCards();
 
 		//Проверяем не пустой ли список
 		if (count_cards === 0) {
@@ -102,144 +187,82 @@ Traveler.prototype = {
 			return false;
 		}
 		if (count_cards === 1) {
-			return this.options['cards'];
+			return this.cards;
 		}
 		
+		//Ассоциативный массив связей
+		depend = {};
 		var first_point = 0;
-		var last_point = 0;
 		for(var i=0; i<count_cards; i++) {
+			//Регитр для индекса, на который не ссылается ни одна карточка
 			var from = 0;
-			var to = 0;
 
 			for(var j=0; j<count_cards; j++) {
 				if (j === i) {
 					continue;
 				}
 				
-				//Находим начальная ли эта точка
-				if (this.options['cards'][i]['from'] === this.options['cards'][j]['to']) {
+				//Если на карточу ссылаются
+				if (this.cards[i]['from'] === this.cards[j]['to']) {
 					from = 1;
+					depend[j] = {'f':j, 's':i};
 				}
-				//Находим конечная ли эта точка
-				/*if (this.options['cards'][i]['to'] === this.options['cards'][j]['from']) {
-					to = 1;
-				}*/
 			}
 			
 			if (from === 0) {
 				first_point = i;
 			}
-			/*if (to === 0) {
-				last_point = i;
-			}*/
-		}
-		
-		var now_point = first_point;
-		for(var i=0; i<count_cards; i++) {
-			sort_cards.push(this.options['cards'][now_point]);
-			for(var j=0; j<count_cards; j++) {
-				if (this.options['cards'][now_point]['to'] === this.options['cards'][j]['from']) {
-					now_point = j;
-					break;
-				}
-			}
-		}
-		
-		return sort_cards;
-	},
-	
-	sort2: function() {
-		//Сортированный список карточек
-		var sort_cards = new Array();
-		
-		var count_cards = this.options['cards'].length;
-
-		//Проверяем не пустой ли список
-		if (count_cards === 0) {
-			throw new Exception('Нет карточек');
-			return false;
-		}
-		if (count_cards === 1) {
-			return this.options['cards'];
-		}
-		
-		por = new Array();
-		var first_point = 0;
-		var last_point = 0;
-		for(var i=0; i<count_cards; i++) {
-			var from = 0;
-			var to = 0;
-
-			for(var j=0; j<count_cards; j++) {
-				if (j === i) {
-					continue;
-				}
-				
-				//Находим начальная ли эта точка
-				if (this.options['cards'][i]['from'] === this.options['cards'][j]['to']) {
-					from = 1;
-					por.push(new Array(j, i));
-				}
-				//Находим конечная ли эта точка
-				/*if (this.options['cards'][i]['to'] === this.options['cards'][j]['from']) {
-					to = 1;
-				}*/
-			}
-			
-			if (from === 0) {
-				first_point = i;
-			}
-			/*if (to === 0) {
-				last_point = i;
-			}*/
 		}
 
 		var now_point = first_point;
-		for(var i=0; i<por.length; i++) {
-			for(var j=0; j<por.length; j++) {
-				if (now_point == por[j][0]) {
-					now_point = por[j][1];
-					break;
-				}
-			}
+		//Количество всегда на 1 меньше, чем количество карточек
+		var depend_length = count_cards - 1;
+		for(var i=0; i<depend_length; i++) {
 			if (i === 0) {
-				sort_cards.push(this.options['cards'][por[j][0]]);
-				sort_cards.push(this.options['cards'][por[j][1]]);
+				//Сохраняем оригинальный индекс для работы с отсортированным списком с доступом к оригинальным
+				this.cards[depend[now_point]['f']]['index'] = now_point;
+				sort_cards.push(this.cards[depend[now_point]['f']]);
+				sort_cards.push(this.cards[depend[now_point]['s']]);
 			} else {
-				sort_cards.push(this.options['cards'][por[j][1]]);
+				sort_cards.push(this.cards[depend[now_point]['s']]);
 			}
+			
+			now_point = depend[now_point]['s'];
 		}
-		
+		a = sort_cards;
 		return sort_cards;
 	},
 	
 	/* Построить карту путешествия */
 	buildTravelCard: function() {
+		var count_cards = this.getCountCards();
+
 		console.time("sort");
 		var sort_cards = this.sort();
 		console.timeEnd("sort");
-		
+
 		var html = '<ul>';
-		for(var i=0; i<sort_cards.length; i++) {
+		for(var i=0; i<count_cards; i++) {
 			html += "<li>";
-			html += "Take <b>"+sort_cards[i]['type_transport']+"</b>";
-			html += " from <b>"+sort_cards[i]['from']+"</b>";
-			html += " to <b>"+sort_cards[i]['to']+"</b>";
-			html += "</li>";
-		}
-		html += "</ul>";
-		document.querySelector('#travel').innerHTML = html;
-		
-		console.time("sort2");
-		var sort_cards = this.sort2();
-		console.timeEnd("sort2");
-		
-		var html = '<ul>';
-		for(var i=0; i<sort_cards.length; i++) {
-			html += "<li>";
-			html += "Take <b>"+sort_cards[i]['type_transport']+"</b>";
-			html += " from <b>"+sort_cards[i]['from']+"</b>";
-			html += " to <b>"+sort_cards[i]['to']+"</b>";
+			html += "Взять <b>"+sort_cards[i]['transport']+"</b>";
+			html += " из <b>"+sort_cards[i]['from']+"</b>";
+			html += " в <b>"+sort_cards[i]['to']+".</b>";
+			
+			if (sort_cards[i]['seat'] && sort_cards[i]['seat'] !== '') {
+				html += " Место <b>"+sort_cards[i]['seat']+".</b>";
+			} else {
+				html += " Место не указано.";
+			}
+			if (typeof(sort_cards[i]['options']) === 'object') {
+				for(var key in sort_cards[i]['options']) {
+					if (typeof(sort_cards[i]['options'][key]) !== 'object') {
+						continue;
+					}
+					if (sort_cards[i]['options'][key]['display']) {
+						html += " "+sort_cards[i]['options'][key]['name']+" <b>"+sort_cards[i]['options'][key]['description']+".</b>";
+					}
+				}
+			}
 			html += "</li>";
 		}
 		html += "</ul>";
